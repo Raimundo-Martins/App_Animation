280bad01450c78cbb1f91b2197c446fe5a1f5559
Raimundo-Martins <cosmomartinsj@gmail.com>
2021-02-05 20:59:30 -0300

    Atualizando

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,44 @@
+import 'package:animations_flutter/screens/login/widgets/form_container.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/background.jpg'), fit: BoxFit.cover),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 70, bottom: 32),
+                      child: Image.asset(
+                        'images/tickicon.png',
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    FormContainer(),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
